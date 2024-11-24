9d17c894a74f094b78b59d389e35a7cf0fb252cd
Arthur Rios Ribeiro <arthurriosribeiro@gmail.com>
2024-11-24 11:58:55 -0300

    Criação da página de portfolio e ajustes de layout

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -2,8 +2,24 @@ class Navigation {
     constructor() {}
 
     navigate(page) {
+        const portfolioPage = "portfolio.html";
         const iframe = parent.document.getElementById("intern-navigation");
-        iframe.src = page;
+
+        iframe.classList.add("iframe-hidden");
+        
+
+        if(page === portfolioPage) {
+            parent.document.body.classList.add("fade-out");
+            setTimeout(() => {
+                parent.window.location.href = portfolioPage;
+            }, 300);
+        } else {
+            iframe.addEventListener("transitionend", function onFadeOut() {
+                iframe.src = page;
+                iframe.classList.remove("iframe-hidden");
+                iframe.removeEventListener("transitionend", onFadeOut);
+            }, {once: true});
+        }
     }
 }
 
